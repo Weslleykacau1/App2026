a4c1d3169a584a93f1a12cbd9a1b74eba66319fd
Weslley Sniper <weslley.kacau@gmail.com>
2025-07-14 13:11:22 +0000

    atualize u supabase: https://fyrlctyzrzwrqbhmnpef.supabase.co  API:eyJhb

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // IMPORTANT: These variables should be stored in your .env.local file.
 // See the Supabase documentation for how to find these values.
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://fyrlctyzrzwrqbhmnpef.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5cmxjdHl6cnp3cnFiaG1ucGVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0MTE3NzAsImV4cCI6MjA2Nzk4Nzc3MH0.HLnPkMUIqiofPOxhmanmFj-F38W7EpbJaoT8STVlmaM'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)

64213065da1f8c5630a74849435c107ee39f0f03
Weslley Sniper <weslley.kacau@gmail.com>
2025-07-16 21:38:50 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="o

--- src/app/driver/on-ride/page.tsx
+++ src/app/driver/on-ride/page.tsx
@@ -122,12 +122,6 @@ function OnRidePage() {
     window.open(`https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`, '_blank');
   };
   
-  const handleOpenWhatsApp = () => {
-    if (!rideData || !rideData.passenger.phone) return;
-    const passengerPhone = rideData.passenger.phone.replace(/\D/g, ''); // Remove non-digit characters
-    window.open(`https://wa.me/${passengerPhone}`, '_blank');
-  };
-  
   const handleOpenChat = () => {
     toast({
         title: "Em breve",
@@ -247,9 +241,6 @@ function OnRidePage() {
               <Button variant="outline" size="icon" className="h-12 w-12 rounded-full" onClick={handleOpenChat}>
                 <MessageCircle />
               </Button>
-               <Button variant="outline" size="icon" className="h-12 w-12 rounded-full" onClick={handleOpenWhatsApp}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
-              </Button>
             </div>
           </CardContent>
         </Card>
@@ -314,7 +305,3 @@ function OnRidePage() {
 }
 
 export default withAuth(OnRidePage, ["driver"]);
-
-    
-
-    
